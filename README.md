243ea439744cf795f4e25b1cba824fbbb2346ede
fabidop <fabidop@gmail.com>
2016-04-20 18:58:00 +0200

    -- fileupload -> send to microsoft -> get response b -- by dopler

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # emojify
-KWM
+a KWM project by fabian dopler, kerstin mayrhofer, christina knorr, lisa lausecka
+
+# Inspirationsquelle
+https://www.microsoft.com/cognitive-services/en-us/face-hero
+
+# Zustände
+"anger":
+"contempt":
+"disgust":
+"fear":
+"happiness":
+"neutral":
+"sadness":
+"surprise":
--- script.js
+++ script.js
@@ -1,3 +1,70 @@
-/**
- * Created by fabidop on 20.04.16.
- */
+'use strict';
+
+// den ganzen scheiss erst ausführen wenn alles geladen (sonst kann man noch nicht auf DOM Elemente zugreifen)
+window.onload = function () {
+
+    // call `readURL` function whenever user chooses image
+    $('#file').change(function(){
+        readURL(this);
+    });
+
+    // take image from file input and show in <img> element
+    function readURL(input) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#preview').attr('src', e.target.result);
+            sendToMicrosoft(dataURItoBlob(e.target.result));
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+
+    // send to microsoft api
+    function sendToMicrosoft (imgStream) {
+        $.ajax({
+                url: "https://api.projectoxford.ai/emotion/v1.0/recognize",
+                beforeSend: function (xhrObj) {
+                    // Request headers
+                    //xhrObj.setRequestHeader("Content-Type","application/json");
+                    xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+                    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "ef69165828f145d4bee5383abcc96755");
+                },
+                type: "POST",
+                crossDomain: true,
+                processData: false,
+                //dataType: 'jsonp',
+                // Request body
+                data: imgStream,
+                //data: '{ "url": "https://static1.squarespace.com/static/524417d8e4b03b33d728eead/t/55f0269ae4b0e4faa5a3f12f/1441801887153/strong_family.png" }'
+            })
+            .done(function (data) {
+                console.log("##### WEBREQUEST SUCCESS: RESPONSE: #####");
+                console.log(data);
+                draw(data);
+            })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                console.log("##### WEBREQUEST FAILED : Output: #####");
+                console.log(textStatus);
+            });
+    }
+
+    // api benötigt binary file -> eingelesen wurde DataURI -> umwandeln
+    function dataURItoBlob(dataURI) {
+        var binary = atob(dataURI.split(',')[1]);
+        var array = [];
+        for(var i = 0; i < binary.length; i++) {
+            array.push(binary.charCodeAt(i));
+        }
+        return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
+    }
+
+    //draw img @postion from response
+    function draw (data) {
+
+    }
+
+};
+
+
+
